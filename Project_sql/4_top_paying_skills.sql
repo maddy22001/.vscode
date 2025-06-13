0433a3c10e0f6568be663a19c28325bf5255c036
kunda <maddy22001@gmail.com>
2025-06-13 23:43:13 +0530

    modified:   Project_sql/3_top_demanded_skills.sql 	new file:   Project_sql/4_top_paying_skills.sql 	new file:   Project_sql/5_optimal_skills.sql

--- Project_sql/4_top_paying_skills.sql
+++ Project_sql/4_top_paying_skills.sql
@@ -1,21 +1,20 @@
 SELECT 
     skills,
-    COUNT(skills_job_dim.job_id) AS demand_count
-
+    ROUND(AVG(salary_year_avg),0) as avg_salary
 FROM job_postings_fact
 
 INNER JOIN skills_job_dim ON  job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 
 WHERE
-    job_title_short = 'Data Analyst' AND
-    job_work_from_home = TRUE
+    job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+    AND job_work_from_home = TRUE
 
 GROUP BY
     skills
 
 ORDER BY
-    demand_count DESC
+    avg_salary DESC
 
-LIMIT 5;
-
+LIMIT 25
