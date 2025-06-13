96211f9cd7ef2aa886a074aec6e225820d7fba2d
kunda <maddy22001@gmail.com>
2025-06-13 12:48:50 +0530

    push example

--- sql_course.session.sql
+++ sql_course.session.sql
@@ -1,16 +1,6 @@
-WITH remote_job_skills AS (
-SELECT
-    skill_id,
-    COUNT(*) AS skill_count
-FROM
-    skills_job_dim AS skills_to_job
+SELECT *
+FROM job_postings_fact
 
-INNER JOIN job_postings_fact AS job_postings ON job_postings.job_id = skills_to_job.job_id
-WHERE
-    job_postings.job_work_from_home = true
+ORDER BY job_id
 
-GROUP BY
-    skill_id)
-
-
-LIMIT 100
+LIMIT 5
